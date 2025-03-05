62b64c22c79e12c51cc8216c2b900eb3ca927e28
Gabriel Stamford Arnaud <137704884+GabrielStamfordDev@users.noreply.github.com>
2025-03-05 15:21:54 -0300

    Update creation_tables.sql

--- creation_tables.sql
+++ creation_tables.sql
@@ -53,10 +53,50 @@ SELECT codigo_disciplina.NEXTVAL, 'SQL Avançado', 60, REF(c)
 FROM tb_curso c
 WHERE c.nome_curso = 'Banco de Dados';
 /
--- printanfo o que adicionei em curso
+-- printando o que adicionei em curso
 SELECT * FROM tb_curso;
 -- printando o que adicionei em disicplina
 SELECT d.codigo_disciplina, d.nome_disciplina, d.carga_horaria, DEREF(d.curso_ref).nome_curso AS nome_curso
 FROM Disciplina d;
 
+-- Criando o tipo de objeto sala. Para isso, preciso antes, criar o tipo de objeto local, que é composto.
+CREATE OR REPLACE TYPE local_Sala_tp AS OBJECT(
+    predio VARCHAR2(20),
+    num_sala VARCHAR2(4)
+);
+/
 
+-- Vou adicionar um ORDER para ordenar as salas de acordo com a capacidade
+CREATE OR REPLACE TYPE sala_tp AS OBJECT(
+    local local_Sala_tp,
+    capacidade NUMBER,
+    ORDER MEMBER FUNCTION comparaCapacidade(S sala_tp) RETURN INTEGER
+);
+/
+
+--Criando corpo de sala pro order
+
+CREATE OR REPLACE TYPE BODY sala_tp AS ORDER MEMBER FUNCTION comparaCapacidade(S sala_tp) RETURN INTEGER IS
+BEGIN 
+RETURN SELF.capacidade - S.capacidade;
+END;
+END;
+/
+    
+-- Criando a tabela de objeto do tipo sala.
+CREATE TABLE tb_sala OF sala_tp (
+    CONSTRAINT sala_pk PRIMARY KEY (local.predio, local.num_sala)
+);
+/
+
+-- Inserindo em sala para testar
+
+INSERT INTO tb_sala VALUES (sala_tp(local_Sala_tp('Predio E', 'D009'), 50));
+
+--Printando o que adicionei em sala. Por algum motivo, ele não consegue mostrar o objeto composto em si
+
+SELECT * FROM tb_sala;
+
+-- dividindo o objeto composto no print
+
+SELECT S.local.predio, S.local.num_sala, S.capacidade FROM tb_sala S;
