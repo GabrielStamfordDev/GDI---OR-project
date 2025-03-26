33664e728e2af19b5fa9e064292d71335d69c382
Gabriel Stamford Arnaud <137704884+GabrielStamfordDev@users.noreply.github.com>
2025-03-25 22:17:16 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -10,4 +10,17 @@ SELECT d.codigo_disciplina, d.nome_disciplina, DEREF(d.curso_ref).nome_curso AS 
 CONSULTA 2: Printar os cursos que têm mais de 2 alunos
 */
 
-SELECT a.curso.nome_curso AS curso_nome, COUNT(CPF) AS quantidade_alunos FROM Aluno a GROUP BY a.curso.nome_curso HAVING COUNT(*) > 2;
+SELECT DEREF(a.curso).nome_curso AS curso_nome, COUNT(CPF) AS quantidade_alunos FROM Aluno a GROUP BY a.curso.nome_curso HAVING COUNT(*) > 2;
+
+/*
+CONSULTA 3: Consultar todos os professores, que têm supervisores, e seus supervisores
+*/
+
+SELECT P.nome AS Nome_professor, P.CPF AS Cpf_professor, DEREF(P.CPF_supervisor).nome AS Nome_Supervisor, DEREF(P.CPF_supervisor).CPF AS CPF_Supervisor
+FROM Professor P WHERE P.CPF_supervisor IS NOT NULL;
+
+/*
+CONSULTA 4: Consultar quantos professores supervisionados cada professor supervisor tem:
+*/
+
+SELECT s.CPF AS supervisor_cpf, s.nome AS supervisor_nome, COUNT(p.CPF) AS num_supervisadosFROM Professor p JOIN Professor s ON p.CPF_supervisor = REF(s) GROUP BY s.CPF, s.nome;
